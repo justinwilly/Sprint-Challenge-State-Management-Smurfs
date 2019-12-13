1dcd35905fca8d0453dad3bddbd2c4578ef6dbb9
Justin C Williams <45780143+justinwilly@users.noreply.github.com>
2019-12-13 13:46:47 -0500

    mvp complete

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Smurfs(props){
+
+    console.log(props.smurfs)
+
+    return(
+        <div>
+            <h3>Name: {props.smurfs.name}</h3>
+            <p>Height: {props.smurfs.height}</p>
+            <p>Age: {props.smurfs.age}</p>
+        </div>
+    )
+}
+
+export default Smurfs;
